d7422f8182a709ca4f6ab35f8ff60d09d9ac9312
jjvega86 <jj.vega86@gmail.com>
2020-08-29 15:49:42 -0400

    Created UserInterface class to handle user input and prompts

---
+++ BattleshipProject/UserInterface.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipProject
+{
+    class UserInterface
+    {
+        //handles all user input and prompts
+
+        public static string GetUserInputString(string prompt)
+        {
+            Console.WriteLine(prompt);
+            string input = Console.ReadLine();
+            return input;
+        }
+
+        public static int GetUserInputInt(string prompt)
+        {
+            Console.WriteLine(prompt);
+            int input = int.Parse(Console.ReadLine());
+            return input;
+        }
+    }
+}
